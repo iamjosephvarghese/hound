97c736a193ced81d829688f1d26dc8176043f929
Paul Alex <paul.alex.pax@gmail.com>
2016-07-20 02:02:46 +0530

    added achievements and media

--- js/scripts.js
+++ js/scripts.js
@@ -197,27 +197,6 @@ $(document).ready(function(){
 
 
 
-	/***************** Fancybox ******************/
-$(document).ready(function(){
-	$(".youtube-media").on("click", function(e) {
-		var jWindow = $(window).width();
-		if (jWindow <= 768) {
-			return;
-		}
-		$.fancybox({
-			href: this.href,
-			padding: 4,
-			type: "iframe",
-			'href': this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
-		});
-		return false;
-	});
-
-});
-
-
-
-
 	//Navigation animated scroll
 	$(document).on('click', 'header nav ul li a', function(event){
 		event.preventDefault()
