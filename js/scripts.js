7709df523e368b31d0194b3e79d47a981468e704
Paul Alex <paul.alex.pax@gmail.com>
2016-07-20 00:20:36 +0530

    somethings added.!

--- js/scripts.js
+++ js/scripts.js
@@ -32,6 +32,7 @@ $(document).ready(function() {
 	}, {
 		offset: '75%'
 	});
+	
 
 });
 
@@ -196,7 +197,23 @@ $(document).ready(function(){
 
 
 
+	/***************** Fancybox ******************/
+$(document).ready(function(){
+	$(".youtube-media").on("click", function(e) {
+		var jWindow = $(window).width();
+		if (jWindow <= 768) {
+			return;
+		}
+		$.fancybox({
+			href: this.href,
+			padding: 4,
+			type: "iframe",
+			'href': this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
+		});
+		return false;
+	});
 
+});
 
 
 
